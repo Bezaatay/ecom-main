65cf8cc2736e88f453d7ef8a265c99fbfa607442
Beyza Atay <133115182+Bezaatay@users.noreply.github.com>
2024-11-21 11:35:23 +0300

    calendar module added

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
 import AppNavigation from './src/navigation';
 import Loader from './src/components/loader/component';
+import { Button, NativeModules } from 'react-native';
 
 export default function App() {
+
+  const { CalendarModule } = NativeModules;
+
+  const onPress = () => {
+    CalendarModule.createCalendarEvent('testName', 'testLocation');
+  };
+
   return (
     <>
-      <AppNavigation />
-      <Loader />
+      {/* <AppNavigation />
+      <Loader /> */}
+      <Button title='Click Me' onPress={onPress} />
     </>
   );
 }
